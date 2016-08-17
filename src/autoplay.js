541a3bfcb15553da00410de1c9c71b6ebd4df5d5
King <yao_jin90@163.com>
2016-08-17 18:05:27 +0800

    shell-cfg son

--- src/autoplay.js
+++ src/autoplay.js
@@ -5,12 +5,12 @@ var path = require('path');
 var fs = require('fs');
 var _ = require('underscore');
 
-var shellConfig = fs.readFileSync(path.join(__dirname, './shell/shell-config.json'), 'utf8')
-shellConfig = JSON.parse(shellConfig);
+
 
 var handleMsg = function(msg) {
     if (msg.from_user.account === 787207323) {
-
+        var shellConfig = fs.readFileSync(path.join(__dirname, './shell/shell-config.json'), 'utf8')
+        shellConfig = JSON.parse(shellConfig);
         _.each(shellConfig, function(val, key) {
             if (msg.content && (msg.content == val)) {
                 require(path.join(__dirname, './shell/' + key))();
