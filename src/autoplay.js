54118737623924d9b2b84068c44e862f8bff10f9
King <yao_jin90@163.com>
2016-08-17 18:21:01 +0800

    clear require cache

--- src/autoplay.js
+++ src/autoplay.js
@@ -5,21 +5,22 @@ var path = require('path');
 var fs = require('fs');
 var _ = require('underscore');
 
-
-
 var handleMsg = function(msg) {
     if (msg.from_user.account === 787207323) {
         var shellConfig = fs.readFileSync(path.join(__dirname, './shell/shell-config.json'), 'utf8')
         shellConfig = JSON.parse(shellConfig);
         _.each(shellConfig, function(val, key) {
             if (msg.content && (msg.content == val)) {
-                require(path.join(__dirname, './shell/' + key))();
+                var shellPath = path.join(__dirname, './shell/' + key);
+                shellPath += '.js';
+
+                //clear require cache
+                delete require.cache[shellPath];
+                require(shellPath)();
             }
         });
     }
 }
-
-
 
 module.exports = function(msg) {
     log.info(JSON.stringify(msg));
