04cb45afda01c2c6615b16d614593d4d25834d2e
King <jinyao@bestpay.com.cn>
2018-11-21 16:42:18 +0800

    [fixbug]: 1.去掉多余字符

--- src/qqauth-qrcode.js
+++ src/qqauth-qrcode.js
@@ -107,7 +107,7 @@ function getUserHome() {
             path: '/ptqrlogin?' + generateUrlParam(),
             headers: {
                 'Cookie': client.get_cookies_string(),
-                'User-Agent': ('Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0'),
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:27.0) Gecko/20100101 Firefox/27.0',
                 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
                 'Referer': 'https://ui.ptlogin2.qq.com/cgi-bin/login?daid=164&target=self&style=16&mibao_css=m_webqq&appid=501004106&enable_qlogin=0&no_verifyimg=1&s_url=http%3A%2F%2Fw.qq.com%2Fproxy.html&f_url=loginerroralert&strong_login=1&login_state=10&t=20131024001'
             }
